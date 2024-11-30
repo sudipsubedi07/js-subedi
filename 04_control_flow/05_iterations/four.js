f47091637ed51d4d4ec175f9192deef265617351
yessudip <134482625+sudipsubedi07@users.noreply.github.com>
2024-11-30 04:45:53 +0000

    loops discussion

---
+++ 04_control_flow/05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "java",
+    swift: "swift by asus"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "c++", "py", "java", "c#"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('NEP', "Nepal")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('NEP', "Nepal")
+
+// for (const key in map) {
+//     console.log(key);
+// }
